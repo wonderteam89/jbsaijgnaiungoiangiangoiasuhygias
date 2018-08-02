81d46eb2ea5df2b6ccf9ba3959a67871d571f97b
lfffftttttttt hgghghghgfd <41709729+wonderteam89@users.noreply.github.com>
2018-08-02 18:43:02 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const ms = require("ms");
+const fs = require('fs');
+const moment = require('moment');
+const request = require('request');
+var Canvas = require('canvas')
+var jimp = require('jimp')
+const pretty = require('pretty-ms');
+const prefix = "#";
+
+const developers = ["316324088865882142","",""]
+const adminprefix = "#";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'watch')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'listen')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'stream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+client.login(process.env.BOT_TOKEN);
